df4eb89c539054f843b8ce35db5278371ffc0aa5
Jeremy Zevin <jzevin@nbme.org>
2022-08-24 12:08:21 -0400

    rmv comments

--- src/app/counter.component.ts
+++ src/app/counter.component.ts
@@ -12,25 +12,18 @@ import { Observable, of } from "rxjs";
 export class CounterComponent {
   count$: Observable<number> = of(0);
   constructor(private store: Store<{ count: number }>) {
-    // TODO: Connect `this.count$` stream to the current store `count` state
     this.count$ = this.store.select("count");
   }
  
   increment() {
-    // TODO: Dispatch an increment action
-    console.log("increment");
     this.store.dispatch(increment());
   }
  
   decrement() {
-    // TODO: Dispatch a decrement action
-    console.log("decrement");
     this.store.dispatch(decrement());
   }
  
   reset() {
-    // TODO: Dispatch a reset action
-    console.log("reset");
     this.store.dispatch(reset());
   }
 }
